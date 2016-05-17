e276a8028b40e6ae096d38b1a8743949a042d0e4
Em An <study.eman@gmail.com>
2016-05-17 07:16:15 +0200

    Create content.php

---
+++ content.php
@@ -0,0 +1,85 @@
+<div class="container">
+	<section style="direction: <?php echo lang('DIRECTION'); ?>">
+		<h2><?php echo lang('DOCUMENTATION'); ?></h2>
+		<ul>
+			<li class="title"><?php echo lang('INTRODUCTION'); ?>
+				<ol>
+					<li><?php echo lang('CREATEECOMMERCE'); ?></li>
+					<li><?php echo lang('MULTILINGUAL'); ?></li>
+					<li><?php echo lang('RESPONSIVE'); ?></li>
+					<li><?php echo lang('PLAINPHP'); ?></li>
+					<li><?php echo lang('USEJQUERY'); ?></li>
+					<li><?php echo lang('PHPDATAOBJECT'); ?></li>
+				</ol>
+			</li>
+		</ul>
+		<ul>
+			<li class="title"><?php echo lang('BASICS'); ?>
+				<ol>
+					<li>HTML</li>
+					<li>CSS</li>
+					<li>CSS3</li>
+					<li>HTML5</li>
+					<li>jQuery</li>
+					<li>Bootstrap</li>
+					<li>PHP</li>
+					<li>mySQL</li>
+					<li>PDO</li>
+				</ol>
+			</li>
+		</ul>
+		<ul>
+			<li class="title"><?php echo lang('USEREDITOR'); ?>
+				<ol>
+					<li>AptanaStudio</li>
+					<li><?php echo lang('OR'); ?> Netbeans</li>
+					<li><?php echo lang('OR'); ?> PHP Strom﻿</li>
+					<li><?php echo lang('OR'); ?> Sublime</li>
+				</ol>
+			</li>
+		</ul>
+		<ul>
+			<li class="title"><?php echo lang('SERVERUSED'); ?>
+				<ol>
+					<li>xampp</li>
+				</ol>
+			</li>
+		</ul>
+		<ul>
+			<li class="title"><?php echo lang('ACTIONSTEPS'); ?>
+				<ol>
+					<li><?php echo lang('REQUIREDFILES'); ?>
+						<p>C: → xampp → htdocs → eCommerce</p>
+					</li>
+					<li><?php echo lang('CALLLIBRARIES'); ?>
+						<ol type="A">
+							<li>Bootstrap<br>
+								<a href="http://blog.getbootstrap.com/2015/11/24/bootstrap-3-3-6-released/">blog.getbootstrap.com</a>
+							</li>
+							<li>
+								Font Awesome<br>
+								CDN <a href="https://cdn.fontawesome.com/">cdn.fontawesome</a><br>
+								icons <a href="http://fontawesome.io/icons/">icons</a><br>
+								<textarea><script src="https://use.fontawesome.com/1740f432cc.js"></script></textarea>
+							</li>
+							<li>
+								jQuery <br>
+								<a href="https://blog.jquery.com/2016/02/22/jquery-1-12-1-and-2-2-1-released/">jquery-1-12-1-and-2-2-1-released</a><br>
+								<a href="https://code.jquery.com/jquery-1.12.1.min.js">jquery-1.12.1.min.js</a>
+							</li>
+						</ol>
+					</li>
+					<li><?php echo lang('LANGUAGEFUNCTION'); ?></li>
+					<li><?php echo lang('CREATEDATABASE'); ?></li>
+					<li><?php echo lang('CONNECTDATABASE'); ?></li>
+					<li><?php echo lang('LOGINFORMDESIGN'); ?></li>
+					<li><?php echo lang('JQUERYPLACEHOLDER'); ?></li>
+					<li><?php echo lang('INITIALIZED'); ?></li>
+					<li><?php echo lang('SESSIONSTART'); ?></li>
+					<li><?php echo lang('CALLNOTNONAVBAR'); ?></li>
+					<li><?php echo lang('TASKLANGCHANGE'); ?></li>
+				</ol>
+			</li>
+		</ul>
+	</section>
+</div>
